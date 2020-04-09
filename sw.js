d4fdab1e9494ffa43aa8a583265babaa9447fb4c
angyongen <angyongen@gmail.com>
2020-04-09 19:52:08 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -56,21 +56,19 @@ self.addEventListener('install', function(event) {
   
 });
 
-function cacheIfValid(response) { //returns promise
-  // Check if we received a valid response
-  if(!response || response.status !== 200 || response.type !== 'basic') return response;
-  // IMPORTANT: Clone the response. A response is a stream and because we want the browser to consume the response
-  // as well as the cache consuming the response, we need to clone it so we have two streams.
-  var responseToCache = response.clone();
-  caches.open(FETCH_CACHE).then(function(cache) {cache.put(event.request, responseToCache);});
-  return response;
-}
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(updateToLatestVersion().then(function() {
     return caches.match(event.request).then(function(response) {
       if (response) return response; // Cache hit - return response
-      return fetch(event.request).then(cacheIfValid);
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if(!response || response.status !== 200 || response.type !== 'basic') return response;
+        // IMPORTANT: Clone the response. A response is a stream and because we want the browser to consume the response
+        // as well as the cache consuming the response, we need to clone it so we have two streams.
+        var responseToCache = response.clone();
+        caches.open(FETCH_CACHE).then(function(cache) {cache.put(event.request, responseToCache);});
+        return response;
+      });
     })
     
   }))

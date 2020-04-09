a038831e318178b850d7d0ecc1ca74895bd69c7b
angyongen <angyongen@gmail.com>
2020-04-09 17:41:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@ function clearOldCaches() {
 function forceCheckVersion() {
     lastVersionCheck = new Date;
     console.log('Checking version...');
-    fetch('/cachewhitelist.txt')
+    fetch('/cachewhitelist.txt?time=' + lastVersionCheck.getTime())
     .then((response) => {
           console.log(response);
       return response.json();

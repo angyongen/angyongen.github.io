282fec0883ea27bf725498e82441ad3a564621c9
angyongen <angyongen@gmail.com>
2020-04-09 17:26:41 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,22 +16,14 @@ function clearOldCaches() {
 
 function forceCheckVersion() {
     console.log('Checking version...');
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/cachewhitelist.txt", true);
-    xhr.onload = function (e) {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-
-          console.log(xhr.responseText);
-        } else {
-          console.error(xhr.statusText);
-        }
-      }
-    };
-    xhr.onerror = function (e) {
-      console.error(xhr.statusText);
-    };
-    xhr.send(null); 
+    fetch('/cachewhitelist.txt')
+    .then((response) => {
+          console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 }
 
 function checkVersion() {

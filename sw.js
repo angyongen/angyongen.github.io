83b4b419f0e42eecf6e5fb4951b9bb8f1beb01e6
angyongen <angyongen@gmail.com>
2020-08-27 11:52:03 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var version = 3;
+var version = 0; //starting cache version 0, should update with cache.txt
 var FETCH_CACHE;
 var cacheWhitelist;
 function updateCacheNames() {
@@ -29,60 +29,61 @@ function getMyCacheVersion() {
 }
 
 async function updateToLatestVersion() {
-  await getMyCacheVersion();
-  if (lastVersionCheck && (new Date - lastVersionCheck) < 30000) return;
-  lastVersionCheck = new Date;
-  var response = await fetch('/cache.txt?time=' + lastVersionCheck.getTime());//prevent disk cache
-  if (response.ok) {
-    var json = await response.json();
-    var newversion = parseInt(json.version);
-    var newurls = response.extraUrlsToCache;
-      try {
-        if (version > newversion) {
-          console.log('WARNING: current sw version greater than cache.txt version');
-        }
-        if (version < newversion) {
-          console.log('New version found, updating...');
-          version = newversion;
-          updateCacheNames();
-          clearOldCaches();
-          extraUrlsToCache = json.extraUrlsToCache
-          caches.open(FETCH_CACHE).then(function(cache) {cache.addAll(extraUrlsToCache);})
-          caches.open(FETCH_CACHE).then(function(cache) {return cache.addAll(urlsToCache);});
-        }
-    } catch (e) {console.error(e)}
-  }
+	await getMyCacheVersion();
+	if (lastVersionCheck && (new Date - lastVersionCheck) < 30000) return;
+	lastVersionCheck = new Date;
+	var response = await fetch('/cache.txt?time=' + lastVersionCheck.getTime());//prevent disk cache
+	if (response.ok) {
+		try {
+			var json = await response.json();
+			var newversion = parseInt(json.version);
+			var newurls = response.extraUrlsToCache;
+			if (version > newversion) {
+				console.log('WARNING: current cache version greater, not updating.');
+			}
+			if (version < newversion) {
+				console.log('New cache version found, updating...');
+				updateCacheNames();
+				clearOldCaches();
+				extraUrlsToCache = json.extraUrlsToCache
+				caches.open(FETCH_CACHE).then(function(cache) {
+					cache.addAll(extraUrlsToCache);
+					cache.addAll(urlsToCache);
+				})
+				version = newversion;
+			}
+		} catch (e) {console.error(e)}
+	}
 }
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
   event.waitUntil(updateToLatestVersion().then(function() {
-    caches.open(FETCH_CACHE).then(function(cache) {cache.addAll(extraUrlsToCache);})
-    caches.open(FETCH_CACHE).then(function(cache) {return cache.addAll(urlsToCache);});
+    caches.open(FETCH_CACHE).then(function(cache) {
+    	cache.addAll(extraUrlsToCache);
+    	cache.addAll(urlsToCache);
+	})
   }));
   
 });
 
 self.addEventListener('fetch', function(event) {
 	event.respondWith(updateToLatestVersion().then(function() {
-		return caches.open(FETCH_CACHE).then(function(cache) {
-			return cache.match(event.request).then(function(cacheresponse) {
-				return fetch(event.request).then(function (fetchresponse) {
-					if(!fetchresponse || fetchresponse.status !== 200 || fetchresponse.type !== 'basic') return fetchresponse;
-					// IMPORTANT: Clone the response. A response is a stream and because we want the browser to consume the response
-					// as well as the cache consuming the response, we need to clone it so we have two streams.
-					var responseToCache = fetchresponse.clone();
-					{cache.put(event.request, responseToCache);};
-					return fetchresponse;
-				}).catch(function(errorresponse) {
-					console.log(errorresponse)
-					console.log(cacheresponse)
-					if (cacheresponse) {
-						return cacheresponse; // Cache hit - fetch error - return cache
-					}
-				});
+		return fetch(event.request).then(function (fetchresponse) {
+			if(!fetchresponse || fetchresponse.status !== 200 || fetchresponse.type !== 'basic') return fetchresponse;
+			// IMPORTANT: Clone the response. A response is a stream and because we want the browser to consume the response
+			// as well as the cache consuming the response, we need to clone it so we have two streams.
+			var responseToCache = fetchresponse.clone();
+			{cache.put(event.request, responseToCache);};
+			return fetchresponse;
+		}).catch(function(errorresponse) {
+			return caches.open(FETCH_CACHE).then(function(cache) {
+				return cache.match(event.request).then(function(cacheresponse) {
+					if (cacheresponse){return cacheresponse; // Cache hit - fetch error - return cache
+				}}
 			})
-		})
+				
+		});
     }))
 })
       
